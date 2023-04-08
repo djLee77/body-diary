d0a5d93b4af5edc9074775a3e347918ddc49c015
djLee77 <dkdnl232@gmail.com>
2023-04-08 21:45:44 +0900

    로그인/회원가입 완성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
 import Login from './components/Login'
 import SignUp from './components/SignUp'
-
 function App() {
   return (
-    <div>
+    <div>  
       <Login/>
       <SignUp/>
     </div>
